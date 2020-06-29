d02fd6a4ea6305da00a830b64a4b27b4b144517f
vuehrjer5 <46229630+vuehrjer5@users.noreply.github.com>
2020-06-29 20:57:11 +0200

    deleted unused code

--- waves.py
+++ waves.py
@@ -1,5 +1,3 @@
-"""Animation zur Entstehung des Doppler-Effekts und des
-machschen Kegels. """
 
 import numpy as np
 import matplotlib as mpl
@@ -10,25 +8,19 @@ import matplotlib.animation
 dt = 0.005
 
 # Dargestellter Ortsbereich 0..+xmax, 0..+ymax [m].
-xlim = (-2.0, 4.0)
+xlim = (-1.5, 4.0)
 ylim = (-2.0, 2.0)
 
-# Frequenz, mit der die Wellenzüge aussendet werden [Hz].
-f_Q = 5.0
 
 # Ausbreitungsgeschwindigkeit der Welle [m/s].
-c = 2.5
+c = 3.5
 
-
-# Lege die Geschwindigkeit von Quelle und Beobachter fest [m/s].
-v_Q = np.array([0, 0])
-v_B = np.array([0, 0])
+# Slit width
+d = 2
 
 # Erzeuge eine Figure und eine Axes.
 fig = plt.figure(figsize=(12, 6))
 ax = fig.add_subplot(1, 1, 1)
-ax.set_xlabel('x [m]')
-ax.set_ylabel('y [m]')
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
 ax.set_aspect('equal')
@@ -50,24 +42,18 @@ for i in range(-8,40,2):
                                     color='blue', fill=True, zorder=4)    
         beobach.center = np.array([i * 0.1, j * 0.1])
         beobach.set_gid((i*10)+j)
-        if not ((i == -2 or i == 0) and not (j < 2 and j > -2)):
+        if not ((i == -2 or i == 0) and not (j < d and j > -d)):
             grid.append(beobach)
             ax.add_patch(beobach)
 
-# Beschriftung.
-ax.text(0.05, 0.95, f'$v_Q = {np.linalg.norm(v_Q) / c:.2f} c$',
-        transform=ax.transAxes, color='black', size=12,
-        horizontalalignment='left', verticalalignment='top')
-ax.text(0.95, 0.95, f'$v_B = {np.linalg.norm(v_B) / c:.2f} c$',
-        transform=ax.transAxes, color='blue', size=12,
-        horizontalalignment='right', verticalalignment='top')
+
 
 # Lege eine Liste an, die die kreisförmigen Wellenzüge speichert.
 kreise = []
 
 for quelle in quellen:
     kreis = mpl.patches.Circle(quelle.center, radius=0,
-                            color='red', linewidth=1.5,
+                            color='blue', linewidth=1.5,
                             fill=False, zorder=3)
     kreis.startzeit = 0
     kreis.set_gid(-999)
@@ -79,8 +65,6 @@ def update(n):
     t = dt * n
 
     # Berechne die aktuelle Position von Quelle und Beobachter.
-
-
 
     # Erzeuge zum Startzeitpunkt einen neuen Kreis oder wenn
     # seit dem Aussenden des letzten Wellenzuges mehr als eine
@@ -116,11 +100,11 @@ def update(n):
     # Färbe den Quelle rot, wenn ein Wellezug augesendet wird.
 
     
-    d = np.linalg.norm(kreise[-1].center - quelle.center)
-    if abs(d - kreise[-1].radius) < quelle.radius:
-        quelle.set_color('red')
-    else:
-        quelle.set_color('black')
+    #d = np.linalg.norm(kreise[-1].center - quelle.center)
+    #if abs(d - kreise[-1].radius) < quelle.radius:
+        #quelle.set_color('red')
+    #else:
+        #quelle.set_color('black')
 
     return kreise 
 
