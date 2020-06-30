bdc000d2d7ba7dd9ff853a5b0cc1fd883494fbc6
vuehrjer5 <46229630+vuehrjer5@users.noreply.github.com>
2020-06-30 16:34:25 +0200

    translated to english

--- waves.py
+++ waves.py
@@ -4,111 +4,85 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation
 
-# Zeitschrittweite [s].
+# timesteps
 dt = 0.005
 
-# Dargestellter Ortsbereich 0..+xmax, 0..+ymax [m].
+# visualized aread
 xlim = (-1.5, 4.0)
 ylim = (-2.0, 2.0)
 
 
-# Ausbreitungsgeschwindigkeit der Welle [m/s].
+# wave 
 c = 3.5
 
-# Slit width
+# Slit width (only put even numbers here)
 d = 2
 
-# Erzeuge eine Figure und eine Axes.
 fig = plt.figure(figsize=(12, 6))
 ax = fig.add_subplot(1, 1, 1)
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
 ax.set_aspect('equal')
-quellen = []
+sources = []
 
-#wall1 = mpl.patches.Rectangle((0.5, 0.5 ),0.2, 0.2, color='black', fill=True, zorder=10)
-#ax.add_patch(wall1)
-# Erzeuge zwei Kreise für Quelle und Beobachter.
+
+# Generate sources
 for i in range(16):
-    quelle = mpl.patches.Circle((0, 0), radius=0.03,
+    source = mpl.patches.Circle((0, 0), radius=0.03,
                             color='black', fill=True, zorder=4)
-    quelle.center= np.array([-1, 1.5-i*0.2])
-    quellen.append(quelle)
-    ax.add_patch(quelle)
+    source.center= np.array([-1, 1.5-i*0.2])
+    sources.append(source)
+    ax.add_patch(source)
 grid = []
 for i in range(-8,40,2):
     for j in range(-15,16,2):
-        beobach = mpl.patches.Circle((0, 0), radius=0.01,
+        point = mpl.patches.Circle((0, 0), radius=0.01,
                                     color='blue', fill=True, zorder=4)    
-        beobach.center = np.array([i * 0.1, j * 0.1])
-        beobach.set_gid((i*10)+j)
+        point.center = np.array([i * 0.1, j * 0.1])
+        point.set_gid((i*10)+j)
         if not ((i == -2 or i == 0) and not (j < d and j > -d)):
-            grid.append(beobach)
-            ax.add_patch(beobach)
+            grid.append(point)
+            ax.add_patch(point)
 
+waves = []
 
-
-# Lege eine Liste an, die die kreisförmigen Wellenzüge speichert.
-kreise = []
-
-for quelle in quellen:
-    kreis = mpl.patches.Circle(quelle.center, radius=0,
+for source in sources:
+    wave = mpl.patches.Circle(source.center, radius=0,
                             color='blue', linewidth=1.5,
                             fill=False, zorder=3)
-    kreis.startzeit = 0
-    kreis.set_gid(-999)
-    kreise.append(kreis)
-    ax.add_patch(kreis)
+    wave.startzeit = 0
+    wave.set_gid(-999)
+    waves.append(wave)
+    ax.add_patch(wave)
 
 def update(n):
-    # Berechne den aktuellen Zeitpunkt.
+    # Calculate current time
     t = dt * n
 
-    # Berechne die aktuelle Position von Quelle und Beobachter.
-
-    # Erzeuge zum Startzeitpunkt einen neuen Kreis oder wenn
-    # seit dem Aussenden des letzten Wellenzuges mehr als eine
-    # Periodendauer vergangen ist.
-
-    #for point in grid:        
-        #point.set_color('blue')
-
-    # Aktualisiere die Radien aller dargestellen Kreise.
-    for kreis in kreise:
-        kreis.radius = (t - kreis.startzeit) * c
-        if kreis.radius > 0.29 and len(kreise) > 0:
-            kreise.pop(kreise.index(kreis))
-    # Färbe den Beobachter rot, wenn ein Wellenzug auftrifft.
-    for kreis in kreise:
-        for beobach in grid:
-            d = np.linalg.norm(kreis.center - beobach.center)
-            if abs(d - kreis.radius) < beobach.radius and kreis._gid != beobach._gid:
-                if(beobach.get_facecolor()[2] == 1):
-                    kreis = mpl.patches.Circle(beobach.center, radius=0,
+    # Update waves
+    for wave in waves:
+        wave.radius = (t - wave.startzeit) * c
+        if wave.radius > 0.29 and len(waves) > 0:
+            waves.pop(waves.index(wave))
+    # When a wave hits a point, the point should send out a new wave (only once)
+    for wave in waves:
+        for point in grid:
+            d = np.linalg.norm(wave.center - point.center)
+            if abs(d - wave.radius) < point.radius and wave._gid != point._gid:
+                if(point.get_facecolor()[2] == 1):
+                    wave = mpl.patches.Circle(point.center, radius=0,
                         color='blue', linewidth=1.5,
                         fill=False, zorder=3)
-                    kreis.startzeit = t
-                    kreis.set_gid(beobach._gid)
-                    kreise.append(kreis)
-                    if(len(kreise) > 500):
-                        kreise.pop(0)
-                    ax.add_patch(kreis)     
-                beobach.set_color('red')                        
+                    wave.startzeit = t
+                    wave.set_gid(point._gid)
+                    waves.append(wave)
+                    if(len(waves) > 500):
+                        waves.pop(0)
+                    ax.add_patch(wave)     
+                point.set_color('red')                        
+    return waves
 
 
-
-    # Färbe den Quelle rot, wenn ein Wellezug augesendet wird.
-
-    
-    #d = np.linalg.norm(kreise[-1].center - quelle.center)
-    #if abs(d - kreise[-1].radius) < quelle.radius:
-        #quelle.set_color('red')
-    #else:
-        #quelle.set_color('black')
-
-    return kreise 
-
-# Erstelle die Animation und zeige die Grafik an.
 ani = mpl.animation.FuncAnimation(fig, update,
                                   interval=30, blit=True)
 plt.show()
